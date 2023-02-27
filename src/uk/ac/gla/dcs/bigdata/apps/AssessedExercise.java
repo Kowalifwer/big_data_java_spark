66b956c1923d8dfaf5c73dc1ce350b95d06b6e71
Artem Grechushkin <artem.grechushkin@mail.ru>
2023-02-27 15:46:25 +0000

    one run on big dataset with output to GL

--- src/uk/ac/gla/dcs/bigdata/apps/AssessedExercise.java
+++ src/uk/ac/gla/dcs/bigdata/apps/AssessedExercise.java
@@ -86,12 +86,11 @@ public class AssessedExercise {
 		// Get the location of the input queries
 		String queryFile = System.getenv("bigdata.queries");
 		if (queryFile==null) queryFile = "data/queries.list"; // default is a sample with 3 queries
-        // UNCOMMENT TO USE CUSTOM QUERY LISTif (queryFile==null) queryFile = "data/queries_custom.list";
 		
 		// Get the location of the input news articles
 		String newsFile = System.getenv("bigdata.news");
-		if (newsFile==null) newsFile = "data/TREC_Washington_Post_collection.v3.example.json"; // default is a sample of 5000 news articles
-		// if (newsFile==null) newsFile = "data/TREC_Washington_Post_collection.v2.jl.fix.json"; // default is a sample of 600,000 news articles
+		// if (newsFile==null) newsFile = "data/TREC_Washington_Post_collection.v3.example.json"; // default is a sample of 5000 news articles
+		if (newsFile==null) newsFile = "data/TREC_Washington_Post_collection.v2.jl.fix.json"; // sample of 600,000 news articles
 		
         // Call the student's code
 		List<DocumentRanking> results = rankDocuments(spark, queryFile, newsFile);
